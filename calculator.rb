121c0adb52335422d873b66654915cf65422203e
Adam D. White <adamwhite82@gmail.com>
2017-10-13 14:17:26 -0500

    completed calculator exercises, performed p/f comparisons

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,19 @@
 class Calculator
     
-        def add(a, b) 
-          a + b
-        end
-        
+    def add(a, b) 
+        a + b
     end
+       
+    def sub(a, b) 
+        a - b
+    end
+       
+    def mult(a, b) 
+        a * b
+    end
+          
+    def div(a, b) 
+        a / b
+    end
+           
+end
--- test_calculator.rb
+++ test_calculator.rb
@@ -13,13 +13,22 @@ class TestCalculator < Minitest::Test
   def test_add
     assert_equal 4, @calc.add(2,2)
   end
-  
+
   # Write test methods for subtract, multiply, and divide
+  def test_sub
+    assert_equal 7, @calc.sub(10,3)
+  end
+
+  def test_mult
+    assert_equal 4, @calc.mult(2,2)
+  end
+
+  def test_div
+    assert_equal 0, @calc.div(2,2)
+  end
 
   def teardown
     puts "tear down"
   end
-
-  
 
 end
